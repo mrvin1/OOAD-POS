4b8474a73f7b01b78d7cfb13d88dc05ec74fb1f1
mrvin1 <vincent.fanditama@gmail.com>
2021-06-16 09:33:19 +0700

    ghhg

--- src/TransactionManagement.java
+++ src/TransactionManagement.java
@@ -4,7 +4,7 @@ public class TransactionManagement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("asdas");
+		System.out.println("asdadfs");
 		
 	}
 
