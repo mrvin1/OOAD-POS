174fe6b7745d26ee83f7f1327f86806ea8abd55b
mrvin1 <vincent.fanditama@gmail.com>
2021-06-16 09:26:28 +0700

    Update TransactionManagement.java

--- src/TransactionManagement.java
+++ src/TransactionManagement.java
@@ -1,13 +1,10 @@
 
 public class TransactionManagement {
 
-	public TransactionManagement() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
